9ec3c872dab683dba810c9b26ac2d60701e841eb
nicolalandro <nicolaxx94@live.it>
2021-02-24 18:01:08 +0100

    train vangog script

--- train_vangogh.sh
+++ train_vangogh.sh
@@ -2,8 +2,8 @@
 
 EPOCHS=200
 DATASET="/home/super/Datasets/vangogh2photo"
-OUTPUT="/home/super/Models/cyclegan/vangog"
+OUTPUT="/home/super/Models/cyclegan/vangogh"
 GPU=0
 
-mkdir -r -p $OUTPUT
+mkdir -p $OUTPUT
 CUDA_VISIBLE_DEVICES="${GPU}" python3 train.py --n_epochs="${EPOCHS}" --dataroot="${DATASET}" --output-folder="${OUTPUT}"
