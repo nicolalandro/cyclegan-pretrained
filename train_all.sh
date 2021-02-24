89d96249096d96b4755b9951064bf9a0975cd5f6
nicolalandro <nicolaxx94@live.it>
2021-02-24 15:49:36 +0100

    run all script also for 1 epoch

--- train_all.sh
+++ train_all.sh
@@ -10,19 +10,19 @@ EPOCHS=1
 DATASET="/media/mint/Barracuda/Datasets/CycleGanDatasets/cezanne2photo"
 OUTPUT="/media/mint/Barracuda/Models/cyclegan/cezanne"
 
-python3 train.py --n_epochs="${EPOCHS}" --dataroot="${DATASET}" --output-folder="${OUTPUT}" --decay_epoch=0
+python3 train.py --n_epochs="${EPOCHS}" --dataroot="${DATASET}" --output-folder="${OUTPUT}"
 
 DATASET="/media/mint/Barracuda/Datasets/CycleGanDatasets/monet2photo"
 OUTPUT="/media/mint/Barracuda/Models/cyclegan/monet"
 
-python3 train.py --n_epochs="${EPOCHS}" --dataroot="${DATASET}" --output-folder="${OUTPUT}" --decay_epoch=0
+python3 train.py --n_epochs="${EPOCHS}" --dataroot="${DATASET}" --output-folder="${OUTPUT}"
 
 DATASET="/media/mint/Barracuda/Datasets/CycleGanDatasets/ukiyoe2photo"
 OUTPUT="/media/mint/Barracuda/Models/cyclegan/ukiyoe"
 
-python3 train.py --n_epochs="${EPOCHS}" --dataroot="${DATASET}" --output-folder="${OUTPUT}" --decay_epoch=0
+python3 train.py --n_epochs="${EPOCHS}" --dataroot="${DATASET}" --output-folder="${OUTPUT}"
 
 DATASET="/media/mint/Barracuda/Datasets/CycleGanDatasets/maps"
 OUTPUT="/media/mint/Barracuda/Models/cyclegan/maps"
 
-python3 train.py --n_epochs="${EPOCHS}" --dataroot="${DATASET}" --output-folder="${OUTPUT}" --decay_epoch=0
+python3 train.py --n_epochs="${EPOCHS}" --dataroot="${DATASET}" --output-folder="${OUTPUT}"
--- utils.py
+++ utils.py
@@ -89,7 +89,7 @@ class ReplayBuffer():
 
 class LambdaLR():
     def __init__(self, n_epochs, offset, decay_start_epoch):
-        assert ((n_epochs - decay_start_epoch) > 0), "Decay must start before the training session ends!"
+        # assert ((n_epochs - decay_start_epoch) > 0), "Decay must start before the training session ends!"
         self.n_epochs = n_epochs
         self.offset = offset
         self.decay_start_epoch = decay_start_epoch
